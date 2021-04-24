017542bbfb9a34228936041f6db43eb4b484d15e
Ariful Islam Arif <arif14arif15@gmail.com>
2021-04-24 16:48:42 +0600

    testimonial form design fix

--- resources/views/admin/testimonial/create.blade.php
+++ resources/views/admin/testimonial/create.blade.php
@@ -48,40 +48,44 @@
             <div class="page-wrapper">
                 <div class="page-body">
                     <div class="row justify-content-center">
-                        <div class="col-sm-8">
+                        <div class="col-sm-12">
 
                             <div class="card">
                                 <div class="card-header">
                                     <h5>Add Testimonial</h5>
                                 </div>
                                 <div class="card-block">
-                                    <form id="main"  method="POST" action="{{ route('testimonial.store') }}">
-                                        @csrf
-                                        <div class="row justify-content-center">
-                                            <div class="col-12">
-                                                <div class="form-group">
-                                                    <label class="col-sm-2 col-form-label">Name</label>
-                                                    <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" placeholder="Enter name">
-                                                     @error('name') <span class="invalid-feedback" role="alert">{{ $message }}</span> @enderror
+                                    <div class="row justify-content-center">
+                                        <div class="col-6">
+                                            <form id="main"  method="POST" action="{{ route('testimonial.store') }}">
+                                                @csrf
+                                                <div class="row justify-content-center">
+                                                    <div class="col-12">
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 col-form-label">Name</label>
+                                                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" placeholder="Enter name">
+                                                             @error('name') <span class="invalid-feedback" role="alert">{{ $message }}</span> @enderror
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 col-form-label">Position</label>
+                                                            <input type="text" class="form-control @error('position') is-invalid @enderror" name="position" id="position" placeholder="Enter position">
+                                                             @error('position') <span class="invalid-feedback" role="alert">{{ $message }}</span> @enderror
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 col-form-label">Content</label>
+                                                            <textarea type="text" class="form-control @error('content') is-invalid @enderror" name="content" id="content" placeholder="Write testimonial content here.... " rows="5"></textarea>
+                                                             @error('content') <span class="invalid-feedback" role="alert">{{ $message }}</span> @enderror
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="col-12">
-                                                <div class="form-group">
-                                                    <label class="col-sm-2 col-form-label">Position</label>
-                                                    <input type="text" class="form-control @error('position') is-invalid @enderror" name="position" id="position" placeholder="Enter position">
-                                                     @error('position') <span class="invalid-feedback" role="alert">{{ $message }}</span> @enderror
-                                                </div>
-                                            </div>
-                                            <div class="col-12">
-                                                <div class="form-group">
-                                                    <label class="col-sm-2 col-form-label">Content</label>
-                                                    <textarea type="text" class="form-control @error('content') is-invalid @enderror" name="content" id="content" placeholder="Write testimonial content here.... " rows="5"></textarea>
-                                                     @error('content') <span class="invalid-feedback" role="alert">{{ $message }}</span> @enderror
-                                                </div>
-                                            </div>
+                                                <button type="submit" class="btn btn-primary  m-b-0"><i class="fas fa-plus"></i> Add</button>
+                                            </form>
                                         </div>
-                                        <button type="submit" class="btn btn-primary  m-b-0"><i class="fas fa-plus"></i> Add</button>
-                                    </form>
+                                    </div>
                                 </div>
                             </div>
 
