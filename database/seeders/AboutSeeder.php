226669cb4cb833b424e5e4a594b125590617c5cc
Zakir Hossen <web.zakirbd@gmail.com>
2021-04-26 10:44:55 +0600

    seeder file updated

--- database/seeders/AboutSeeder.php
+++ database/seeders/AboutSeeder.php
@@ -16,6 +16,7 @@ class AboutSeeder extends Seeder
     {
         About::create([
             'content' => 'Zakir Soft is a Software company and Software Development Training Institute. We develop Softwares for your business and we also provide quality software training in Adabor Since 2020. We have passionate teams like Full Stack Laravel Developer, Android Developer, UI/UX Designer. We have management system softwares for the local market.
+
             we also have eCommerce systems for your online business.'
         ]);
     }
